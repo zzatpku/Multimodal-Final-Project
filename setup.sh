6b8841c36e62e85024d2f9292ea2f4c68394f79d
Zhang Zhang <zzhang@stu.pku.edu.cn>
2025-01-10 20:06:17 +0800

    The initial commit

---
+++ setup.sh
@@ -0,0 +1,12 @@
+conda create -n pointllm python=3.10 -y
+conda activate pointllm
+conda install cudatoolkit=11.8 -y
+conda install nvidia/label/cuda-11.8.0::cuda -y
+conda install pytorch==2.0.1 torchvision==0.15.2 torchaudio==2.0.2 pytorch-cuda=11.8 -c pytorch -c nvidia -y
+pip install transformers==4.28.0
+pip install -e .
+pip install ninja
+pip install flash-attn==2.5.9post1
+pip install opencv-python
+pip install numpy==1.26.4
+
